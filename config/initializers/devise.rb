53f91695c81c4c824ba885d83834698451136529
vagrant <vagrant@precise32.(none)>
2014-04-16 02:58:07 +0000

    add

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -7,6 +7,7 @@ Devise.setup do |config|
   if Rails.env.production?
     config.secret_key = ENV['DEVISE_SECRET']
   end
+  
   # ==> Mailer Configuration
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
